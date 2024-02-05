8e4e1215a24db469fd9c96595ec4266d666c821f
S29 <nots29vv@gmail.com>
2024-02-05 19:16:51 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,21 @@ import type { Metadata } from 'next';
 
 const frameMetadata = getFrameMetadata({
   buttons: [
-      {label: 'Click to unlock', action: 'post_redirect'}
+      {label: 'Send', action: 'post'},
+      {label: 'Balance', action: 'post'},
+      {label: 'Deposit or Withdraw', action: 'post_redirect'}
   ],
   image: 'https://rd-orpin.vercel.app/video.png',
   post_url: 'https://rd-orpin.vercel.app/api/frame',
 });
 
 export const metadata: Metadata = {
-  title: 'Redirect frame',
-  description: 'LFG',
+  title: 'Socket Pay',
+  description: 'On-chain payments powered by Socket Bridge API',
   openGraph: {
-    title: 'Redirect frame',
-    description: 'LFG',
-    images: ['https://rd-orpin.vercel.app//video.png'],
+    title: 'Socket Pay',
+    description: 'A frame facilitating payments between facaster IDs',
+    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmZPrZ45GrnmjbGw6Xj27mzgpju7FCguKAbwBkUVxBTPVB`],
   },
   other: {
     ...frameMetadata,
@@ -25,7 +27,8 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <img src={'/video.png'}/>
+      <h1>Socket Pay</h1>
     </>
   );
 }
+          
