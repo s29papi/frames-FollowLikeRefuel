ba72a49df944cadf174d72b4c3dcff47784d9d36
S29 <nots29vv@gmail.com>
2024-02-06 00:31:39 +0100

    adding destination chain selection

--- app/api/send-frame/route.ts
+++ app/api/send-frame/route.ts
@@ -5,7 +5,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   const id:any = searchParams.get("id")
   const idAsNumber = parseInt(id)
 
-  const nextId = idAsNumber + 1
+  let nextId = idAsNumber + 1
 
   if(idAsNumber === 1){
       return new NextResponse(`<!DOCTYPE html><html><head>
@@ -18,10 +18,32 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     <meta property="fc:frame:button:2:action" content="post" />
     <meta property="fc:frame:button:3" content="Optimism" />
     <meta property="fc:frame:button:3:action" content="post" />
-    <meta property="fc:frame:button:2" content="Starknet" />
-    <meta property="fc:frame:button:2:action" content="post" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
+    <meta property="fc:frame:post_url" content="https://socket-pay.vercel.app/api/send-frame?id=${nextId}" />
   </head></html>`);
+  } 
+
+  nextId = idAsNumber + 1
+
+  if(idAsNumber === 2){
+    const data = await req.json();
+    const buttonId = data.untrustedData.buttonIndex;
+    if (buttonId === 1){ 
+      console.log(data)
+    }
+    if (buttonId === 2){ 
+      console.log(data)
+    }
+    if (buttonId === 3){ 
+      console.log(data)
+    }
+    return new NextResponse(`<!DOCTYPE html><html><head>
+        <title>Frame Sends Eth</title>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://magenta-hollow-tiglon-795.mypinata.cloud/ipfs/QmZPrZ45GrnmjbGw6Xj27mzgpju7FCguKAbwBkUVxBTPVB"/>
+        <meta property="fc:frame:button:1" content="Select Destination Chain" />
+        <meta property="fc:frame:button:1:action" content="post"/>
+        <meta property="fc:frame:post_url" content="https://socket-pay.vercel.app/api/send-frame?id=${nextId}"/>
+      </head></html>`);
   } 
   
   return new NextResponse(`<!DOCTYPE html><html><head>
