37629c79247e005bd754a75df6071126fdf8e76b
S29 <nots29vv@gmail.com>
2024-02-06 17:56:35 +0100

    adding db

--- app/redirect/redirectPage/page.tsx
+++ app/redirect/redirectPage/page.tsx
@@ -4,23 +4,18 @@ import { useSearchParams } from 'next/navigation'
 import {useEffect} from "react";
 
 
-export default async  function RedirectPage() {
+export default function RedirectPage() {
     const router = useRouter();
     const searchParams = useSearchParams()
  
     
-    // console.log(searchParams.get("SourceChainId"))
+    console.log(searchParams.get("SourceChainId"))
 
     useEffect(() => {
-        const redirectUrl = 'https://socket-pay.vercel.app/redirect/signTx';
+        // const redirectUrl = 'https://socket-pay.vercel.app/redirect/signTx';
 
-        // Perform the redirect
-        const sleep = (ms: any) => new Promise(resolve => setTimeout(resolve, ms));
-        sleep(20000000000000).then(() => {
-            console.log('End sleeping');
-            // Perform the redirect after sleep
-            window.location.href = redirectUrl;
-        }); // For a full page reload redirect
+        // // Perform the redirect
+        // window.location.href = redirectUrl; // For a full page reload redirect
         // Or use Next.js router for client-side redirect (comment out the line above if using this)
         // router.push(youtubeUrl);
     }, [router]);
@@ -31,7 +26,3 @@ export default async  function RedirectPage() {
         </div>
     );
 }    
-
-async function sleep(ms: any) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
