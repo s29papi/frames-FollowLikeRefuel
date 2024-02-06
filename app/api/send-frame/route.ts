ae9cf7db1e1fbeb712afd8e12262273324abef74
S29 <nots29vv@gmail.com>
2024-02-06 11:23:40 +0100

    loading ui

--- app/api/send-frame/route.ts
+++ app/api/send-frame/route.ts
@@ -25,6 +25,17 @@ alchemy.core.getBlockNumber().then(console.log);
   const id:any = searchParams.get("id")
   const idAsNumber = parseInt(id)
 
+  if (idAsNumber === 0){
+    return new NextResponse(`<!DOCTYPE html><html><head>
+    <title>Frame Sends Eth</title>
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="https://magenta-hollow-tiglon-795.mypinata.cloud/ipfs/QmZPrZ45GrnmjbGw6Xj27mzgpju7FCguKAbwBkUVxBTPVB"/>
+    <meta property="fc:frame:button:1" content="Select Destination Chain" />
+    <meta property="fc:frame:button:1:action" content="post"/>
+    <meta property="fc:frame:post_url" content="https://socket-pay.vercel.app/api/send-frame?id=1"/>
+  </head></html>`);
+  }
+
   let nextId = idAsNumber + 1
 
   if(idAsNumber === 1){
