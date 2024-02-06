2d4805a4c7e625f36f83ea3e82c57259ef20f2e8
S29 <nots29vv@gmail.com>
2024-02-06 14:15:00 +0100

    adding db

--- app/api/send-frame/route.ts
+++ app/api/send-frame/route.ts
@@ -18,7 +18,7 @@ alchemy.core.getBlockNumber().then(console.log);
 // const signer = provider.getSigner()
 
 
- 
+  let getParams = req.nextUrl.searchParams
   
   const searchParams = req.nextUrl.searchParams
   const id:any = searchParams.get("id")
@@ -41,19 +41,20 @@ alchemy.core.getBlockNumber().then(console.log);
   let nextId = idAsNumber + 1
 
   if(idAsNumber === 1){
-    
+    let destinationAddress = searchParams.get("DestinationAddress");
+    let userInfo = "DestinationAddress=" + destinationAddress; 
       return new NextResponse(`<!DOCTYPE html><html><head>
-    <title>Select the Destination chain...</title>
-    <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="https://magenta-hollow-tiglon-795.mypinata.cloud/ipfs/QmZPrZ45GrnmjbGw6Xj27mzgpju7FCguKAbwBkUVxBTPVB"/>
-    <meta property="fc:frame:button:1" content="Ethereum" />
-    <meta property="fc:frame:button:1:action" content="post" />
-    <meta property="fc:frame:button:2" content="Arbitrum" />
-    <meta property="fc:frame:button:2:action" content="post" />
-    <meta property="fc:frame:button:3" content="Optimism" />
-    <meta property="fc:frame:button:3:action" content="post" />
-    <meta property="fc:frame:post_url" content="https://socket-pay.vercel.app/api/send-frame?id=${nextId}" />
-  </head></html>`);
+          <title>Select the Destination chain...</title>
+          <meta property="fc:frame" content="vNext" />
+          <meta property="fc:frame:image" content="https://magenta-hollow-tiglon-795.mypinata.cloud/ipfs/QmZPrZ45GrnmjbGw6Xj27mzgpju7FCguKAbwBkUVxBTPVB"/>
+          <meta property="fc:frame:button:1" content="Ethereum" />
+          <meta property="fc:frame:button:1:action" content="post" />
+          <meta property="fc:frame:button:2" content="Arbitrum" />
+          <meta property="fc:frame:button:2:action" content="post" />
+          <meta property="fc:frame:button:3" content="Optimism" />
+          <meta property="fc:frame:button:3:action" content="post" />
+          <meta property="fc:frame:post_url" content="https://socket-pay.vercel.app/api/send-frame?id=${nextId}&${userInfo}" />
+        </head></html>`);
   } 
 
   nextId = idAsNumber + 1
