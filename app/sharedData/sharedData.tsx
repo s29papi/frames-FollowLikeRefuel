8319718c2a09500fd6e71c59cafc5350e57fee3a
S29 <nots29vv@gmail.com>
2024-02-06 15:25:07 +0100

    adding db

--- app/sharedData/sharedData.tsx
+++
@@ -1,8 +0,0 @@
-// sharedData.js
-import { useState } from 'react';
-
-export const useSharedData = () => {
-  const [sharedData, setSharedData] = useState({});
-
-  return { sharedData, setSharedData };
-};
