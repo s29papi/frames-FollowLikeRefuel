f7d73d850af2169b99d09978e6c44f153255b661
S29 <nots29vv@gmail.com>
2024-02-10 22:21:09 +0100

    set og params

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -8,10 +8,10 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         <title>Frame </title>
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content="https://magenta-hollow-tiglon-795.mypinata.cloud/ipfs/QmZPrZ45GrnmjbGw6Xj27mzgpju7FCguKAbwBkUVxBTPVB"/>
-        <meta property="fc:frame:input:text" content="Destination Address" />
-        <meta property="fc:frame:button:1" content="Enter" />
+        <meta property="fc:frame:input:text" content="Enter Address" />
+        <meta property="fc:frame:button:1" content="Submit" />
         <meta property="fc:frame:button:1:action" content="post"/>
-        <meta property="fc:frame:post_url" content="https://socket-pay.vercel.app/api/send-frame?id=0"/>
+        <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/send-frame?id=0"/>
       </head></html>`);
 }
 
