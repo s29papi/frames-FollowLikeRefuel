531bf3e22b106ad7427dc1a216686e9d6a815424
S29 <nots29vv@gmail.com>
2024-02-11 12:26:15 +0100

    set og params

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -15,10 +15,22 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
           <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/send-frame?id=0"/>
         </head></html>`);
   }
+
+  // if the interactive user hasn't followed and liked cast return 
+  if (false) {
+          return new NextResponse(`<!DOCTYPE html><html><head>
+          <title>Like & Follow</title>
+          <meta property="fc:frame" content="vNext" />
+          <meta property="fc:frame:image" content="https://magenta-hollow-tiglon-795.mypinata.cloud/ipfs/QmRUEpTXSm2cjuDDgzS4pvXC9E9g2gtXuGVw8D3BCV5iup"/>
+          <meta property="fc:frame:button:1" content="Refuel" />
+          <meta property="fc:frame:button:1:action" content="post"/>
+          <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/send-frame?id=0"/>
+      </head></html>`);
+  }
       return new NextResponse(`<!DOCTYPE html><html><head>
-                <title>Like & Follow</title>
+                <title>Like Cast</title>
                 <meta property="fc:frame" content="vNext" />
-                <meta property="fc:frame:image" content="https://magenta-hollow-tiglon-795.mypinata.cloud/ipfs/QmRUEpTXSm2cjuDDgzS4pvXC9E9g2gtXuGVw8D3BCV5iup"/>
+                <meta property="fc:frame:image" content="https://magenta-hollow-tiglon-795.mypinata.cloud/ipfs/QmfHYnG4NthhjZ684FLNTEtuiymiMcrmoQq164mZYkxKHa"/>
                 <meta property="fc:frame:button:1" content="Refuel" />
                 <meta property="fc:frame:button:1:action" content="post"/>
                 <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/send-frame?id=0"/>
