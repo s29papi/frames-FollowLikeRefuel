901db3059f2055d093a8234707ebf23656e70322
S29 <nots29vv@gmail.com>
2024-02-11 12:41:49 +0100

    set og params

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -27,14 +27,26 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/send-frame?id=0"/>
     </head></html>`);
   }
-      return new NextResponse(`<!DOCTYPE html><html><head>
-                <title>Follow User</title>
-                <meta property="fc:frame" content="vNext" />
-                <meta property="fc:frame:image" content="https://magenta-hollow-tiglon-795.mypinata.cloud/ipfs/QmNskr3tpJpkBmT3aJzocfxgeWHQTjsFTz12Ms6M99w5DP"/>
-                <meta property="fc:frame:button:1" content="Refuel" />
-                <meta property="fc:frame:button:1:action" content="post"/>
-                <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/send-frame?id=0"/>
+
+  if (false) {
+        return new NextResponse(`<!DOCTYPE html><html><head>
+        <title>Follow User</title>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://magenta-hollow-tiglon-795.mypinata.cloud/ipfs/QmNskr3tpJpkBmT3aJzocfxgeWHQTjsFTz12Ms6M99w5DP"/>
+        <meta property="fc:frame:button:1" content="Refuel" />
+        <meta property="fc:frame:button:1:action" content="post"/>
+        <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/send-frame?id=0"/>
     </head></html>`);
+  }
+    return new NextResponse(`<!DOCTYPE html><html><head>
+              <title>Input Wallet Address</title>
+              <meta property="fc:frame" content="vNext" />
+              <meta property="fc:frame:image" content="https://magenta-hollow-tiglon-795.mypinata.cloud/ipfs/QmVQGtDeK7eLFa3oDdqGXF24rvezH4hXkjPkVQSkL22giS"/>
+              <meta property="fc:frame:input:text" content="Wallet Address..." />
+              <meta property="fc:frame:button:1" content="Submit" />
+              <meta property="fc:frame:button:1:action" content="post"/>
+              <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/send-frame?id=0"/>
+  </head></html>`);
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
