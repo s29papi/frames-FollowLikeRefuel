cae8dd54ed64b4b54d0ad3fdd1d5b942371a3161
S29 <nots29vv@gmail.com>
2024-02-12 19:08:20 +0100

    set og params

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -12,38 +12,38 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   if (message?.liked) liked = message?.liked
 
   // if the interactive user hasn't followed and liked cast return 
-//   if (!liked && !following) {
-//           return new NextResponse(`<!DOCTYPE html><html><head>
-//           <title>Like & Follow</title>
-//           <meta property="fc:frame" content="vNext" />
-//           <meta property="fc:frame:image" content="https://frames-follow-like-refuel.vercel.app/second-page.png"/>
-//           <meta property="fc:frame:button:1" content="Refuel" />
-//           <meta property="fc:frame:button:1:action" content="post"/>
-//           <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/frame"/>
-//       </head></html>`);
-//   }
+  if (!liked && !following) {
+          return new NextResponse(`<!DOCTYPE html><html><head>
+          <title>Like & Follow</title>
+          <meta property="fc:frame" content="vNext" />
+          <meta property="fc:frame:image" content="https://frames-follow-like-refuel.vercel.app/second-page.png"/>
+          <meta property="fc:frame:button:1" content="Refuel" />
+          <meta property="fc:frame:button:1:action" content="post"/>
+          <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/frame"/>
+      </head></html>`);
+  }
 // // if the interactive user hasn't liked cast return 
-//   if (!liked && following) {
-//         return new NextResponse(`<!DOCTYPE html><html><head>
-//         <title>Like Cast</title>
-//         <meta property="fc:frame" content="vNext" />
-//         <meta property="fc:frame:image" content="https://frames-follow-like-refuel.vercel.app/fourth-image.png"/>
-//         <meta property="fc:frame:button:1" content="Refuel" />
-//         <meta property="fc:frame:button:1:action" content="post"/>
-//         <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/frame"/>
-//     </head></html>`);
-//   }
+  if (!liked && following) {
+        return new NextResponse(`<!DOCTYPE html><html><head>
+        <title>Like Cast</title>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://frames-follow-like-refuel.vercel.app/fourth-image.png"/>
+        <meta property="fc:frame:button:1" content="Refuel" />
+        <meta property="fc:frame:button:1:action" content="post"/>
+        <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/frame"/>
+    </head></html>`);
+  }
 
-//   if (liked && !following) {
-//         return new NextResponse(`<!DOCTYPE html><html><head>
-//         <title>Follow User</title>
-//         <meta property="fc:frame" content="vNext" />
-//         <meta property="fc:frame:image" content="https://frames-follow-like-refuel.vercel.app/third-page.png"/>
-//         <meta property="fc:frame:button:1" content="Refuel" />
-//         <meta property="fc:frame:button:1:action" content="post"/>
-//         <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/frame"/>
-//     </head></html>`);
-//   }
+  if (liked && !following) {
+        return new NextResponse(`<!DOCTYPE html><html><head>
+        <title>Follow User</title>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://frames-follow-like-refuel.vercel.app/third-page.png"/>
+        <meta property="fc:frame:button:1" content="Refuel" />
+        <meta property="fc:frame:button:1:action" content="post"/>
+        <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/frame"/>
+    </head></html>`);
+  }
 
   if (!message?.interactor.verified_accounts[0]) {
     return new NextResponse(`<!DOCTYPE html><html><head>
