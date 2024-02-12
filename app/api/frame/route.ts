66fd88883fb9ff9b2839e435700a3e8824d496ee
S29 <nots29vv@gmail.com>
2024-02-12 17:23:12 +0100

    set og params

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -67,13 +67,10 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 // </head></html>`);
 
 return new NextResponse(`<!DOCTYPE html><html><head>
-<title>Success Page</title>
-<meta property="fc:frame" content="vNext" />
-<meta property="fc:frame:image" content="https://frames-follow-like-refuel.vercel.app/sixth-image.png"/>
-<meta property="fc:frame:button:1" content="Refuel" />
-<meta property="fc:frame:button:1:action" content="post"/>
-<meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/frame"/>
-</head></html>`);
+  <title>Success Page</title>
+  <meta property="fc:frame" content="vNext" />
+  <meta property="fc:frame:image" content="https://frames-follow-like-refuel.vercel.app/sixth-image.png"/>
+  </head></html>`);
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
