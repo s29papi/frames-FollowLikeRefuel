fbd46e96206631ab38af0f0ebf7230f9ae46d0fe
S29 <nots29vv@gmail.com>
2024-02-12 02:02:21 +0100

    set og params

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -16,7 +16,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
           <title>Like & Follow</title>
           <meta property="fc:frame" content="vNext" />
           <meta property="fc:frame:image" content="https://magenta-hollow-tiglon-795.mypinata.cloud/ipfs/QmYfs8ZD3AGAXanmw1XXnDTWf4qGcueCT1JbJD78aJiLfL"/>
-          <meta property="fc:frame:button:1" content="Refuel" />
+          <meta property="fc:frame:button:1" content="Refuel user address ${message?.interactor.verified_accounts[0]}" />
           <meta property="fc:frame:button:1:action" content="post"/>
           <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/frame"/>
       </head></html>`);
@@ -51,7 +51,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
               <meta property="fc:frame:input:text" content="Wallet Address..." />
               <meta property="fc:frame:button:1" content="Submit" />
               <meta property="fc:frame:button:1:action" content="post"/>
-              <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/frame/refuel?${message?.interactor.verified_accounts[0]}"/>
+              <meta property="fc:frame:post_url" content="https://frames-follow-like-refuel.vercel.app/api/frame/refuel"/>
   </head></html>`);
 }
 
